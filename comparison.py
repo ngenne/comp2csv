ee4d87a3492bcbbe90f1058d21a143cedf657641
Niels <42961385+ngenne@users.noreply.github.com>
2019-12-23 11:54:09 +0100

    Add files via upload

---
+++ comparison.py
@@ -0,0 +1,33 @@
+# coding: utf-8
+"""
+Created on Tue Nov  5 10:44:54 2019
+
+@author: NGENNE
+"""
+
+### COMPARAISON ##
+
+import argparse
+
+parser = argparse.ArgumentParser(description='REMEMBER : You do not have to put ">" after the command line because this program creates already a file named "output.txt" in the current file directory !')
+parser.add_argument('file1', type=str, help='Needs to write first file path you want to compare as initial')
+parser.add_argument('file2', type=str, help='Needs to write second file path you want to compare as final')
+
+args = parser.parse_args()
+
+import sys
+import difflib
+
+old = open(sys.argv[1], 'r').readlines()
+new = open(sys.argv[2], 'r').readlines()
+
+output = open("output.txt","w") # write mode 
+
+for line in difflib.unified_diff(old, new):
+    output.write(line) 
+
+output.close()
+
+
+
+# si le nombre de lignes --- est égal au nombre de lignes +++, alors il n'y pas de différences entre les deux fichiers
